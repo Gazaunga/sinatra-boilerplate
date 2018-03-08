33c9d768361dbcd23062a44e69557aa1c9218aac
Gazaunga <Gazaunga@gmail.com>
2018-03-08 18:03:33 -0500

    Create app.rb

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'bundler'
+require 'sinatra'
+require './assets'
+require 'haml'
+require 'sass'
+
+Bundler.require(:default)
+
+configure do
+	set :erb, :layout => :layout
+end
+
+## CONTROLLER ##
+
+get '/' do
+	erb :index
+end
+
